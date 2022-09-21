01c6e4004dc81e0cd3972425d1607ecd9be6b9ee
admin <admin@PC22>
2022-09-21 12:44:03 +0900

    commit

--- thisisjava/src/ch05/sec06/ArrayCreateByValueListExample2.java
+++ thisisjava/src/ch05/sec06/ArrayCreateByValueListExample2.java
@@ -15,10 +15,13 @@ public class ArrayCreateByValueListExample2 {
 		}
 		System.out.println("총합 : " + sum1);
 		
-		//printItem()메소드 호출 - 같은 class애의 다른 메소드호출
+		// printItem()메소드 호출 - 같은 class애의 다른 메소드호출
 		printItem(new int[] {83,90,87});
 		
 	}
+	
+	// main메소드가 stack메모리 공간에 있기 때문에 메소드 앞에 stack를 붙여준다.
+	// 메모리 공간에 있어야 바로 사용이 가능함
 	// 전달받은 배열의 각 요소를 출력해주는 메소드
 	static void printItem(int[] scores) {
 		for(int i = 0; i < scores.length; i++) {
